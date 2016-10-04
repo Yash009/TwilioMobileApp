17aa9860d07da2345f6d1a0df34ca5a2841c5591
Yash009 <yash.900209@gmail.com>
2016-10-03 20:15:31 -0400

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Twilio Credentials
 var accountSid = 'AC96ce2a5a8bd543a324809ee09baa8ed1';
-var authToken = '[AuthToken]';
+var authToken = '3555fbf93496d3c3d51bc4262a7ace77';
 
 //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
